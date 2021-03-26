cf64c13484814879c12015e7c7d595ee801fe83a
szumied <szumilas.jakub@gmail.com>
2021-03-26 11:25:58 +0000

    initial code push

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Write a module that will simulate autonomic car.
 The simulation is event based, an example:
+
+```
 car1 = Car()
 car1.act(event)
 print(car1.wheel_angle, car1.speed)
+```
 where event can be anything you want, i.e. :
 `('obstacle', 10)` where `10` is a duration (time) of the event.
 
@@ -27,4 +30,6 @@ When you are done upload this code to github repository.
 Delete these comments before commit!
 Good luck.
     
+---
+
 [![Work in Repl.it](https://classroom.github.com/assets/work-in-replit-14baed9a392b3a25080506f3b7b6d57f295ec2978f6f33ec97e36a161684cbe9.svg)](https://classroom.github.com/online_ide?assignment_repo_id=4439538&assignment_repo_type=AssignmentRepo)
--- task.py
+++ task.py
@@ -1,5 +1,67 @@
+import numpy as np
+from random import randrange
+
+class Simulation():
+    def __init__(self, car, road):
+        self.car = car
+        self.road = road
+
+    def show_car_position():
+        pass
+
+    def Simulate(car, road):
+        pass
+
+class Road:
+    def __init__(self):
+        self.layout = np.zeros((32, 3))
+        self.roads_length = self.layout.shape[0]
+        self.roads_width = self.layout.shape[1]
+        self.generate_obstacles()
+    
+    def generate_obstacles(self, max_count_of_obstacles = 3):
+        obstacles_count = 0
+
+        def generate_obstacle_coordinates():
+            x = randrange(self.roads_width)
+            y = randrange(self.roads_length)
+            return (x,y)
+
+        while (obstacles_count < max_count_of_obstacles):
+            self.layout = self.layout[generate_obstacle_coordinates()] = 1
+        
+        print("Obstacles layout:")
+        self.print_layout()
+
+        print("\nObstacles shape:")
+        print(self.layout.shape)
+
+
+    def print_layout(self):
+        print(self.layout)
+
+
+class Car:
+    def __init__(self):
+        self.wheel_angle = 0  # - 90 left, 90 right, 0 forward
+        self.speed = 0
+        self.position = ((0,1))
+
+    def Drive():
+        pass
+        
+
 def main():
-    pass
+    road = Road()
+    car = Car()
+    road = Road()
+
+    Simulation(car, road)
+
+
+    # Simulate(car, road)
+    
+
 
 if __name__ == "__main__":
     main()
