4ad6fe6beae67fb6f8414b914e0172ebd4310936
szumied <>
2021-04-08 20:01:03 +0000

    Rework task.py rework in separate file

---
+++ task_rework.py
@@ -0,0 +1,57 @@
+from dataclasses import dataclass
+from typing import Callable
+from typing import List
+
+class Car:
+    def __init__(self):
+        self.speed = 0
+        self.wheel_angle = 0
+        self.status = ''
+
+    def print_status(self):
+        print(f"[Speed: {self.speed}, wheel angle: {self.wheel_angle}] {self.status}")
+
+    def update_status(self, status):
+        self.status = status
+        self.print_status()
+
+    def act(self, event):
+        self.update_status(f"**EVENT: {event.type}.**")
+        [action(self) for action in event.actions]
+
+    def set_speed(self, speed):
+        self.speed = speed
+        self.update_status(f"Set speed to {speed}")
+    
+    def modify_speed(self,speed):
+        self.speed = self.speed + speed
+        self.update_status(f"Adjust speed by {speed}")
+    
+    def wait(self, speed):
+        print(f"[Speed: {self.speed}, wheel angle: {self.wheel_angle}] Waiting.")
+
+@dataclass
+class Event:
+    type: str
+    time: int
+    actions: List[Callable]
+
+events_list = [
+    Event('Drive Normally', 0, [
+        lambda car: car.set_speed(50)            
+    ]),
+    Event('Red Light', 3, [
+        lambda car: car.set_speed(0),
+        lambda car: car.wait(10)
+    ]),
+    Event('Fantasize about street racing', 4, [
+        lambda car: car.modify_speed(100)
+    ])
+]
+        
+def main():
+    car1 = Car()
+    [car1.act(event) for event in events_list]
+    
+if __name__ == "__main__":
+    main()
